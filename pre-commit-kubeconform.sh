080324b15118e45f6aadedfe1198506938a8a68f
Ian Homer <code@ianhomer.com>
2025-06-26 18:22:05 +0100

    fix: add kubeconform for pre-commit

---
+++ pre-commit-kubeconform.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if ! command -v kubeconform > /dev/null 2>&1 ; then
+  echo "Please install kubeconform - https://github.com/yannh/kubeconform"
+  exit 1
+fi
+
+filename=$1
+
+kubeconform -summary -verbose \
+  -schema-location default -schema-location \
+  'https://raw.githubusercontent.com/datreeio/CRDs-catalog/main/{{.Group}}/{{.ResourceKind}}_{{.ResourceAPIVersion}}.json' \
+  $filename
+
+
+exit $?
